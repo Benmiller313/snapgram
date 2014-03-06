7462346233dc1874db6aeb0d3e98a061b440cc0d
benmiller313 <benmiller313@gmail.com>
2014-03-06 15:34:17 -0700

    Adding test of production database.

--- database.js
+++ database.js
@@ -15,7 +15,7 @@ conn.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
   console.log('The solution is: ', rows[0].solution);
 });
 
-conn.query('CREATE DATABASE test', function(err, rows){
+conn.query('USE s513_bamiller', function(err, rows){
 	console.log(err);
 	console.log(rows);
 	console.log(rows[0]);
